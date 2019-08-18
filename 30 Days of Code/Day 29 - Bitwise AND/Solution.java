d05d872e821a95bbfd4ede907417ee7e11bd7d11
MD SAIF ALAM <38750964+mdsaifalam96@users.noreply.github.com>
2019-08-18 15:33:38 +0530

    Add files via upload

---
+++ 30 Days of Code/Day 29 - Bitwise AND/Solution.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int t = in.nextInt();
+        for(int i = 0; i < t; i++){
+            int n = in.nextInt();
+            int k = in.nextInt();
+            if(((k-1)|k) > n && k%2==0){
+                System.out.println(k-2);
+            }else{
+                System.out.println(k-1);               
+            }
+        }
+    }
+}
